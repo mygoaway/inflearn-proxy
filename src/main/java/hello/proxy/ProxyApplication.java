6fc6d7c30c2f24d60a0efe642d6c6b78208218f6
mygoaway <mygoaway2@gmail.com>
2022-10-27 22:32:32 +0900

    스프링이 제공하는 빈 후처리 강의 후 커밋

--- src/main/java/hello/proxy/ProxyApplication.java
+++ src/main/java/hello/proxy/ProxyApplication.java
@@ -6,6 +6,7 @@ import hello.proxy.config.v2_dynamicproxy.DynamicProxyFilterBasicConfig;
 import hello.proxy.config.v3_proxyfactory.ProxyFactoryConfigV1;
 import hello.proxy.config.v3_proxyfactory.ProxyFactoryConfigV2;
 import hello.proxy.config.v4_postprocessor.BeanPostProcessorConfig;
+import hello.proxy.config.v5_autoproxy.AutoProxyConfig;
 import hello.proxy.trace.logtrace.LogTrace;
 import hello.proxy.trace.logtrace.ThreadLocalLogTrace;
 import org.springframework.boot.SpringApplication;
@@ -21,7 +22,8 @@ import org.springframework.context.annotation.Import;
 //@Import(DynamicProxyFilterBasicConfig.class)
 //Import(ProxyFactoryConfigV1.class)
 //@Import(ProxyFactoryConfigV2.class)
-@Import(BeanPostProcessorConfig.class)
+//@Import(BeanPostProcessorConfig.class)
+@Import(AutoProxyConfig.class)
 @SpringBootApplication(scanBasePackages = "hello.proxy.app") //주의
 public class ProxyApplication {
 
@@ -33,5 +35,4 @@ public class ProxyApplication {
 	public LogTrace logTrace() {
 		return new ThreadLocalLogTrace();
 	}
-
 }
